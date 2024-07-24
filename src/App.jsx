d5f94ed6d22892dee7898901ebb28158a3c42581
Erfanmah7 <efn8mah@yahoo.com>
2024-07-24 22:02:19 +0330

    add tanstack-Query

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import HomePage from "./components/HomePage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient =new QueryClient();
+
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </>
   );
